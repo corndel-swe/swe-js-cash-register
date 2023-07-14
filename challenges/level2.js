8def923ae9714574548bebc6bf4ec352346d2fca
Simon Haines <nimon@hotmail.co.uk>
2023-07-15 00:52:09 +0100

    Update challenges/level2.js

--- challenges/level2.js
+++ challenges/level2.js
@@ -3,7 +3,7 @@ const drawer = require('../drawer')
 // Level 2
 
 /**
- * Goes through the given drawer and coints how many *coins* there are in total
+ * Goes through the given drawer and counts how many *coins* there are in total
  * N.b. just the number of coins, not the total value of them
  * N.b. $1 is a note, not a coin
  * @param {object[]} drawer

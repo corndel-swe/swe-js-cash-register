d225ef70b14d2ab7704d573debaf8ffd30d9dd99
Simon Haines <nimon@hotmail.co.uk>
2023-07-17 09:18:40 +0100

    Update level3.js

--- challenges/level3.js
+++ challenges/level3.js
@@ -4,7 +4,13 @@ const drawer = require('../drawer')
 
 /**
  * Calculates the total value of all money in the drawer
- * @param {object[]} drawer
+ * Give the answer as a string formatted in dollars, i.e. "$23.78" not 2378
+ *
+ * Consider using the .toFixed() method:
+ * https://www.w3schools.com/jsref/jsref_tofixed.asp
+ *
+ * @param {object[]} - drawer
+ * @returns {string} The amount of money in the drawer formatted in dollars
  */
 function sumDrawer(drawer) {
   // code here

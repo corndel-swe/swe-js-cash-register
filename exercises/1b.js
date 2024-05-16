916d6d37b6b40737689b6ef8f10ee3d64144c962
Simon Haines <nimon@hotmail.co.uk>
2024-05-16 11:11:54 +0100

    adds exercises and challenges

---
+++ exercises/1b.js
@@ -0,0 +1,16 @@
+/**
+ * This function changes the recipeBook's name to the newName
+ * and then returns the updated recipeBook
+ * e.g. renameBook(recipeBook, "Good eats") => { title: 'Good eats' }
+ */
+function renameBook(recipeBook, newName) {
+  // code here
+}
+
+// we've provided a sample object for debugging:
+const recipeBook = {
+  title: 'My Recipe Book'
+}
+
+// don't change below
+export default renameBook
---
+++ exercises/2b.js
@@ -0,0 +1,27 @@
+/**
+ * Given an array of recipes, return a new array of strings containing
+ * the title of each recipe
+ */
+function listTitles(recipes) {
+  // code here
+}
+
+// array provided for debugging:
+const recipes = [
+  {
+    name: 'Spaghetti Bolognese',
+    ingredients: ['spaghetti', 'beef', 'tomato sauce'],
+    servings: 4,
+    isVegetarian: false
+  },
+
+  {
+    name: 'Vegetable Stir Fry',
+    ingredients: ['broccoli', 'carrots', 'bell peppers', 'soy sauce'],
+    servings: 2,
+    isVegetarian: true
+  }
+]
+
+// don't change below
+export default listTitles
